2a6c0f7da7d73535c26fc65c7b94051311f6fd03
5montest <5montest.everyday@gmail.com>
2018-12-06 14:07:13 +0900

    Update encoder_read.py

--- Software/server/encoder_read.py
+++ Software/server/encoder_read.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import pigpio
-import time
+import sys
 
 A = 15
 B = 7
@@ -13,12 +13,12 @@ pi = pigpio.pi()
 pi.set_mode(A,pigpio.INPUT)
 pi.set_mode(B,pigpio.INPUT)
 
-pi.set_pull_up_down(A,pigpio.PUD_DOWN)    #A相側のピンに内部プルダウンを有効化
-pi.set_pull_up_down(B,pigpio.PUD_DOWN)    #B相側のピンに内部プルダウンを有効化
+pi.set_pull_up_down(A,pigpio.PUD_UP)    #A相側のピンに内部プルアップを有効化
+pi.set_pull_up_down(B,pigpio.PUD_UP)    #B相側のピンに内部プルアップを有効化
 
 def cb_interrupt():
-    global flg
-    if pi.read(B) == 0: #B相がLOW
+    global rotate
+    if pi.read(B) == 1: #B相がLOW
         if flg == 1:    #かつ前回のB相がLOWなら
             rotate += 1 #回転数+1
 
@@ -26,10 +26,11 @@ def cb_interrupt():
 
     else:   #B相がHIGH
         if flg == 0:        #かつ前回のB相もHIGHなら
-            rotate -= 1     #回転数+1
+            rotate -= 1     #回転数-1
 
         flg = 0         #B相がHIGHなので現状左回転
 
-    print ("\r{0:d}".format(rotate),end="")
+    sys.stdout.write("\r{}".format(rotate))  #回転数を出力
+    sys.stdout.flush()
 
-cb = pi.callback(A,pigpio.RISING_EDGE,cb_interrupt)
+cb = pi.callback(A,pigpio.FALLING_EDGE,cb_interrupt)
